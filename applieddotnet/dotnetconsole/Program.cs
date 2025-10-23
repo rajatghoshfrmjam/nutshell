d7138aef2b24956887d7144b452cf1c4d746b9df
rajatghoshfrmjam <rajatghoshfrmjam@gmail.com>
2025-10-23 14:24:55 +0530

    asp.net core file upload sample

---
+++ applieddotnet/dotnetconsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetconsole
+{
+    internal partial class program
+    {
+        static void Main(string[] args)
+        {
+            memstream();
+            Console.WriteLine();
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey();
+        }
+    }
+}
